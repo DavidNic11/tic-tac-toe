3ae7e889e2c3cf47e286306c92dbe512a77cb272
David Nicholas <Dnicholas@bitovi.com>
2022-09-02 16:24:54 -0700

    update scoreboard

--- src/components/Score/Score.tsx
+++ src/components/Score/Score.tsx
@@ -2,14 +2,31 @@ import { useScore } from "./scoreContext";
 
 import styles from "./Score.module.css";
 
+interface DisplayProps {
+  label: string;
+  wins: number;
+}
+
+const Display = ({ label, wins }: DisplayProps) => {
+  return (
+    <div className={styles.score}>
+      <p className={styles.scoreText}>{wins}</p>
+      <p className={styles.scoreLabel}>{label}</p>
+    </div>
+  );
+};
+
 const Score = () => {
   const { xWins, oWins } = useScore();
 
   return (
     <div className={styles.scoreContainer}>
-      <h2>Number of Wins</h2>
-      <p className={styles.scoreText}>X: {xWins}</p>
-      <p className={styles.scoreText}>O: {oWins}</p>
+      <h2>Score</h2>
+      <div className={styles.scoreRow}>
+        <Display label="X Wins" wins={xWins} />
+        <div className={styles.scoreDivider} />
+        <Display label="O Wins" wins={oWins} />
+      </div>
     </div>
   );
 };
